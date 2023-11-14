8dd7a3f074aee187964de16e4d28779293027968
Aiden Miller <100258120+a-miller77@users.noreply.github.com>
2023-11-14 17:35:25 -0600

    Enemy skeleton complete

---
+++ FlyWeightProject/Enemy.py
@@ -0,0 +1,51 @@
+import pygame
+import math
+from Projectile import Projectile
+from EnemyFactory import EnemyFactory
+from Weapon import normalize_vector
+
+class Enemy(pygame.sprite.Sprite):
+    projectiles = pygame.sprite.Group()
+    def __init__(self, name, pos: tuple[float, float]):
+        super().__init__()
+        flyweight = EnemyFactory.get(name)
+        self.image = flyweight.image
+        self.rect = flyweight.rect
+        self.radius = flyweight.radius
+        self.speed = flyweight.speed
+        self.weapon = flyweight.weapon
+
+        self.name = name
+        self.health = flyweight.default_health
+        self.movement_vector = [0, 0]
+        self.pos = list(pos)
+        self.last_shot_time = pygame.time.get_ticks()
+        
+    def move(self, enemies: pygame.sprite.Group, player_pos: tuple[float, float], tDelta: float):
+        self.movement_vector = (player_pos[0] - self.pos[0],
+                               player_pos[1] - self.pos[1])
+        self.movement_vector = normalize_vector(self.movementVector)
+        self.pos[0] += self.movement_vector[0] * self.speed * tDelta
+        self.pos[1] += self.movement_vector[1] * self.speed * tDelta
+        
+        # Collision test with other enemies
+        self.movementVector = [0, 0]
+        for sprite in enemies:
+            if sprite is self:
+                continue
+            if pygame.sprite.collide_circle(self, sprite):
+                self.movement_vector[0] += self.pos[0] - sprite.pos[0]
+                self.movement_vector[1] += self.pos[1] - sprite.pos[1]
+
+        self.movement_vector = normalize_vector(self.movement_vector)
+        self.pos[0] += self.movement_vector[0] * 0.5  # The constant is how far the sprite will be
+        self.pos[1] += self.movement_vector[1] * 0.5  # dragged from the sprite it collided with
+        
+        self.rect.topleft = self.pos
+
+    def attack(self, target_pos):
+        self.weapon.attack(self, target_pos, self.last_shot_time)
+        self.last_shot_time = pygame.time.get_ticks()
+
+    def render(self, surface):
+        surface.blit(self.image, self.pos)
