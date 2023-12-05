c4b96bd3c2d3a700394f89b58e6b4b6b2dc690a5
rubioreyesa@msoe.edu <115561804+rubioreyesa@users.noreply.github.com>
2023-12-05 00:14:43 -0600

    changes

--- FlyWeightProject/Coin.py
+++ FlyWeightProject/Coin.py
@@ -4,6 +4,7 @@ from FlyWeightProject.CoinFlyweight import CoinFlyweight
 import CoinFactory
 import pygame
 
+<<<<<<< Updated upstream
 class CoinFlyweight():
     def __init__(self, value: int, image: pygame.Surface):
         self.value = value
@@ -17,6 +18,17 @@ class CointFactory():
     @staticmethod
     def get(self, value: int):
         return self.__Coins___.get(value)
+=======
+class CoinFactory:
+    coins__ = {
+        1 : CoinFlyweight(value = 1, image = (3, pygame.Surface([4, 4])))
+        }
+    @staticmethod
+    def get():
+        return CoinFactory.coins__.get(1)
+    
+    
+>>>>>>> Stashed changes
 class Coin(pygame.sprite.Sprite):
     def  collide():
         pass 
--- FlyWeightProject/Player.py
+++ FlyWeightProject/Player.py
@@ -1,19 +1,56 @@
 import pygame
+import math 
+import Weapon
+
+def normalize_vector(vector):
+    if vector == [0, 0]:
+        return [0, 0]    
+    pythagoras = math.sqrt(vector[0]*vector[0] + vector[1]*vector[1])
+    return (vector[0] / pythagoras, vector[1] / pythagoras)
 class Player( pygame.sprite.Sprite):
-    def __init__(self, pos: tuple[float, float],  weapon_name: str):
+    projectiles = pygame.sprite.Group()
+    def __init__(self, pos: tuple[float, float],  weapon_name: str, screenSize):
         super().__init__()
-        self.weapon = WeaponFactory.get(weapon_name)
+        self.rect = self.image.get_rect(x=screenSize[0]//2,
+                                        y=screenSize[1]//2)
+        self.weapon = Weapon.get(weapon_name)
         self.image = pygame.transform.scale(pygame.image.load("FlyWeightProject\Images\player.png"), (100, 100))
         self.pos  = list(pos)
         self.movement_vector = [0, 0]
         self.alive = True
         self.health = 100
-        self.speed = 20
+        self.speed = 3
 
 
-    def move(self, surfaceSize, tDelta):
+    def move(self, screenSize, tDelta):
+        self.movementVector = normalize_vector(self.movementVector)
+        newPos = (self.pos[0] + self.movementVector[0]*self.movementSpeed*tDelta,
+                    self.pos[1] + self.movementVector[1]*self.movementSpeed*tDelta)
+        if newPos[0] < 0:
+            self.pos[0] = 0
+        elif newPos[0] > screenSize[0] - self.rect.width:
+            self.pos[0] = screenSize[0] - self.rect.width
+        else:
+            self.pos[0] = newPos[0]
         
+        if newPos[1] < 0:
+            self.pos[1] = 0
+        elif newPos[1] > screenSize[1]-self.rect.height:
+            self.pos[1] = screenSize[1]-self.rect.width
+        else:
+            self.pos[1] = newPos[1]
+            
+        self.rect.topleft = self.pos
+        self.movementVector = [0, 0]
+
 
     def attack(self, target_pos):
         self.weapon.attack(self, target_pos, self.last_shot_time)
         self.last_shot_time = pygame.time.get_ticks()
+    
+    def collide(self, damage):
+        self.health -= damage
+        return self.health < 0
+        
+    def render(self, surface):
+        surface.blit(self.image, self.pos)
